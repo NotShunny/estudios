a53503ef937f88ad714b75a2e9587119ab7b0046
NotShunny <Brandongameryt1231@gmail.com>
2024-10-13 23:53:38 -0300

    Add files via upload

---
+++ nuevo/script.js
@@ -0,0 +1,19 @@
+let Nombre = 'Bran';
+
+let Apellido = 'Rojas';
+
+let Ubicacion = 'Buenos Aires';
+
+let edad = 6;
+
+let YO = edad + 10;
+
+console.log('Hola soy' , Nombre.concat('don') , Apellido);
+
+console.log('Vivo en', Ubicacion, 'y tengo', YO);
+
+let Nacimiento = '22 de Marzo '
+
+let Fecha = Nacimiento + 'de 2008';
+
+console.log('Naci el' , Fecha );
