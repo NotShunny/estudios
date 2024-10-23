c20a2af69a75fea2fd2c7ca90ee8fe351edf38ae
NotShunny <Brandongameryt1231@gmail.com>
2024-10-23 14:03:41 -0300

    Add files via upload

---
+++ nuevo/ejercicios.js
@@ -0,0 +1,64 @@
+ // Ejercicio 1: Verificación de contraseña
+// Pídele al usuario que ingrese una contraseña. Si la contraseña es correcta (por ejemplo, "1234"), 
+// muestra un mensaje de éxito con alert, si no, muestra un mensaje de error.
+
+// ejercicio 1 
+
+// let contraseña = prompt('Crea una contraseña');
+
+// let confirmar = prompt('Repita su contraseña');
+
+// if (confirmar === contraseña) {
+//     console.log('Contraseña creada exitosamente');
+//     alert('Contraseña asegurada');
+// } else {
+//     console.log('Contraseña incorrecta');
+//     alert('Contraseña incorrecta, pruebe devuelta');
+// }
+
+// Ejercicio 2: Clasificación de edad
+// Pídele al usuario su edad y responde con un mensaje que clasifique su rango de edad:
+// - Menor de 13: "Eres un niño."
+// - Entre 13 y 17: "Eres un adolescente."
+// - Entre 18 y 64: "Eres un adulto."
+// - 65 o más: "Eres un adulto mayor."
+
+// Ejercicio 2 
+
+// let edad = prompt('Cuantos años tenes?');
+
+// if (edad >= 65) { 
+//     alert('Eres una persona mayor');
+// } else if (edad >= 18) {
+//     alert('Eres un adulto');
+// } else if (edad >= 17) {
+//     alert('Sos un adolescente');
+// } else {
+//     alert('Sos un menor de edad');
+// }
+
+// Ejercicio 3: Nmero par o impar
+// Pídele al usuario un número e indicale por mensaje si es par o impar.
+
+// Ejercicio 3 
+
+// let Aleatorio = prompt('Escriba un numero, determinare si es par o impar');
+
+// if (Aleatorio % 2 ==0) {
+//     console.log('El numero es par');
+//     alert('el numero es par');
+// } else {
+//     console.log('el numero es impar');
+//     alert('el numero es impar');
+// }
+
+// Ejercicio 4: Calculadora 
+// Pídele al usuario dos números y la operación que quiere realizar (+, -, *, /). 
+// Dependiendo de la operación, muestra el resultado correspondiente usando alert.
+
+let primer_numero =  prompt('Pon el primer numero');
+
+let segundo_numero = prompt('El segundo numero');
+
+let Calculo = prompt('Que desea realizar? /, +, *, -?');
+
