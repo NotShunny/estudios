33013695623c2833f1dc6166d145aec83a9f6166
NotShunny <Brandongameryt1231@gmail.com>
2024-10-30 15:17:40 -0300

    Update ejercicios.js

--- nuevo/ejercicios.js
+++ nuevo/ejercicios.js
@@ -1,41 +1,18 @@
-// Ejercicio 4 pero con function //
+// let detector = 0;
 
+// while (detector <= 20) {
+//     if (detector % 2 === 0 ){
+//         console.log('Este numero es par ' + detector );
+//     } else {
+//         console.log('Este numero es impar ' + detector );
+//     }
+//     detector = detector + 1;
+// }
 
-let primer_numero = prompt('Pon el primer numero');
-let segundo_numero = prompt('El segundo numero');
-
-let primerNumeroParseado = parseInt(primer_numero);
-let segundoNumeroParseado = parseInt(segundo_numero);
-
-let calculo = prompt('Que desea realizar? /, +, *, - ?');
-
-function sumar(primerNumeroParseado, segundoNumeroParseado) {
-    alert(`La operacion es sumar y el resultado es:  ${primerNumeroParseado + segundoNumeroParseado}`);
-}
-
-function restar(primerNumeroParseado, segundoNumeroParseado) {
-    alert(`La operacion es restar y el resultado es: ${primerNumeroParseado - segundoNumeroParseado}` );
-}
-
-function multiplicar(primerNumeroParseado , segundoNumeroParseado) {
-    alert(`La operacion es multiplicacion y el resultado es : ${primerNumeroParseado * segundoNumeroParseado}`);
-}
-function dividir(primerNumeroParseado , segundoNumeroParseado) {
-    alert(`La operacion es division y el resultado es: ${primerNumeroParseado / segundoNumeroParseado}`);
-}
-// +
-// -
-// *
-// /
-
-if (calculo === '+') {
- sumar(primerNumeroParseado, segundoNumeroParseado);
-} else if (calculo === '-') {
-   restar(primerNumeroParseado, segundoNumeroParseado);
-} else if (calculo === '*') {
- multiplicar(primerNumeroParseado, segundoNumeroParseado);
-} else if (calculo === '/') {
- dividir(primerNumeroParseado, segundoNumeroParseado);
-} else {
-  alert('No existe la operacion que ingresaste...');
-}
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log('Este numero es par ' + i);
+//     } else {
+//         console.log('Este numero es impar ' + i); 
+//     }
+// }
