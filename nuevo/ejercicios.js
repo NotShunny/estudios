b6321e7bdc8caddf3858ed2ac82b03a21398de9e
NotShunny <Brandongameryt1231@gmail.com>
2024-12-13 20:11:17 -0300

    Update ejercicios.js

--- nuevo/ejercicios.js
+++ nuevo/ejercicios.js
@@ -1,33 +1,20 @@
-// // Ejercicio 8: Inventario de productos
-// // Instrucción: Permite al usuario agregar productos a un inventario y luego buscar productos por nombre
+// //ejercicio 9 : Eliminar elementos de un arreglo//
+// //instruccion : permite al usuario eliminar elementos de un arreglo de numeros //
 
-let productos = [];
+// let numeros = [10, 20, 30, 40, 50];
+// console.log('Arreglo inicial:', numeros);
 
-let ingresarProducto = 'Si';
+// let preguntar = parseInt(prompt('¿Cual numero quiere eliminar?'));
 
-let Preguntar = prompt('¿Va a seguir ingresando productos?');
+// let numerosNuevos = [];
 
-while(Preguntar == 'Si') {
-    let ingresar = prompt('¿Que vas a meter?')
-    let precioProducto = prompt('Cual es el precio?')
-    const inventario = {
-        nombre : ingresar,
-        precio : precioProducto
-    }
-    productos.push(inventario);
-    Preguntar = prompt('¿Va a seguir ingresando productos?');
-}
+// for (let i = 0 ; i < numeros.length ; i ++) {
+//     if (numeros[i] !== preguntar) {
+//         numerosNuevos.push(numeros[i]);
+//     }
+// }
+// numeros = numerosNuevos;
+//     console.log(`el numero que fue eliminado es ${preguntar}`);
+//     console.log('Arreglo final:' , numeros);
 
-
-let productoBuscado = prompt('¿Que producto esta buscando?');
-
-
-for(let i = 0; i < productos.length; i++) {
-   
-    if (productoBuscado == productos[i].nombre) {
-        console.log(`el producto buscado es ${productos[i].nombre} y el precio es ${productos[i].precio}`)
-    } else {
-        console.log('El producto buscado no esta');
-    }
-}
 
