036e966d780e734d4fb10783e6d2e2cc5aa63d38
NotShunny <Brandongameryt1231@gmail.com>
2025-03-28 14:47:10 -0300

    Update ejercicios.js

--- nuevo/ejercicios.js
+++ nuevo/ejercicios.js
@@ -1,28 +1,14 @@
-// 7. Fusionar dos arreglos de objetos si se repiten se tienen que sumar la suma.
-// Es decir por ej maria deberia tener cantidad de 8 y asi con todos
-const empleados1 = [
-  { id: 1, nombre: "Juan" , cant: 3 },
-  { id: 2, nombre: "María", cant: 7 },
-  { id: 3, nombre: "Lalo", cant: 7 },
-  { id: 6, nombre: "Nahu", cant: 0 }
-];
-const empleados2 = [
-  { id: 2, nombre: "María" , cant: 1 },
-  { id: 5, nombre: "Carlos", cant: 12  },
-  { id: 3, nombre: "Lalo", cant: 4  }
-];
+let nums = [1, 2, 2, 3, 4, 4, 5];
+//Eliminar los numeros duplicados //
 
-let fusionar = empleados1.concat(empleados2);
+let contador = 0
 
-console.log(fusionar)
+let eliminar = nums.reduce(function (x, currentValue){
+    if (!x.includes(currentValue)){
+       x.push(currentValue)
+    }
+    return x;
+},[])
 
-let reducir = fusionar.reduce(function (acumulador , CurrentValue){
-if (acumulador[CurrentValue.id]) {
-  acumulador[CurrentValue.id].cant += CurrentValue.cant; 
-} else {
-  acumulador[CurrentValue.id] = CurrentValue;
-}
-return acumulador
-}, {});
+console.log(eliminar)
 
-console.log(reducir)
