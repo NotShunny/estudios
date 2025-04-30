5a5a9119c4634a93aa3463b58a92db511fb2829e
NotShunny <Brandongameryt1231@gmail.com>
2025-04-30 15:52:24 -0300

    Update ejercicios.js

--- nuevo/ejercicios.js
+++ nuevo/ejercicios.js
@@ -1,22 +1,72 @@
+// Estructura | Qué es | Cómo accedo
+// Objeto | Datos con claves | objeto.propiedad o objeto["propiedad"]
+// Array | Lista de elementos | array[índice]
+
+
+
+// Agrupar productos por categoría
+// Consigna:
+// Dado un arreglo de productos con "nombre" y "categoria",
+// devolver un objeto donde cada propiedad sea una categoría
+// y el valor un arreglo con los nombres de los productos de esa categoría.
+
 const productos = [
-  { nombre: "iPhone 12", categoria: "Celulares" },
-  { nombre: "iPhone 13", categoria: "Celulares" },
-  { nombre: "MacBook Air", categoria: "Notebooks" },
-  { nombre: "MacBook Pro", categoria: "Notebooks" },
-  { nombre: "iPad", categoria: "Tablets" },
-  { nombre: "iPad Mini", categoria: "Tablets" }
+  { nombre: "Leche", categoria: "Alimentos" },
+  { nombre: "Camiseta", categoria: "Ropa" },
+  { nombre: "Pan", categoria: "Alimentos" },
+  { nombre: "Pantalón", categoria: "Ropa" },
 ];
 
-// devolver un objeto que cada propiedad sea la key, la categoria y el value en un arreglo con los nombres de los productos pertenecientes a esa categoria//
+// Output esperado:
+// {
+//   Alimentos: ["Leche", "Pan"],
+//   Ropa: ["Camiseta", "Pantalón"]
+// }
 
-let arr = [];
+let resultado = {};
 
-for (let i= 0 ; i < productos.length; i++){
-  let recorrer = productos[i]
-  if(!arr[recorrer.categoria]){
-    arr[recorrer.categoria] = []
+for (let i = 0; i < productos.length; i++){
+if (!resultado[productos[i].categoria]){
+resultado[productos[i].categoria] = [productos[i].nombre]
+} else {
+  resultado[productos[i].categoria].push(productos[i].nombre)
+}
+
+};
+
+console.log(resultado)
+
+//////////////////////////////////////////////////////////////////////////.////////////////.////////////////////////./////////////////
+
+// Ejercicio 4 - Contar usuarios por país
+// Consigna:
+// Dado un arreglo de usuarios con propiedad "pais",
+// devolver un objeto con la cantidad de usuarios por cada país.
+
+const usuariosPais = [
+  { nombre: "María", pais: "Argentina" },
+  { nombre: "John", pais: "USA" },
+  { nombre: "Lucía", pais: "Argentina" },
+  { nombre: "Carlos", pais: "México" },
+];
+
+
+// Output esperado:
+// {
+//   Argentina: 2,
+//   USA: 1,
+//   México: 1
+// }
+
+
+let objetos = {}
+
+for (let i = 0; i < usuariosPais.length; i++){
+  if (objetos[usuariosPais[i].pais]){
+    objetos[usuariosPais[i].pais] = objetos[usuariosPais[i].pais] +1
+  } else {
+    objetos[usuariosPais[i].pais] = 1
   }
-  arr[recorrer.categoria].push(recorrer.nombre);
-  }
+}
 
-console.log(arr)
+console.log(objetos)
